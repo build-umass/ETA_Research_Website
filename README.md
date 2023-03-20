31af7fcd0de9a2f6823f7e127a5b7657d5735216
samonuall <samonuall@gmail.com>
2023-03-19 21:20:38 -0400

    Change README, add React basic app

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ETA_Research_Website
 We will be using a React frontend and NodeJS backend for this project. \
-Client contact: b.davis@umass.edu
+Client contact: b.davis@umass.edu \
+
+# Setting up the project
+Download Node and NPM on to your device, link: https://nodejs.org/   \
+To run the server, run the command "npm start" in your terminal \ 
+To test out what the server is doing, past this link into your browser: http://localhost:3001/api
+
--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
+// set port variable (where server will listen from)
 const PORT = process.env.PORT || 3001;
 
+// using MongoDB express module
 const app = express();
+
+// Sets up behavior for when a client sends a GET request, res is the response we want to send
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
