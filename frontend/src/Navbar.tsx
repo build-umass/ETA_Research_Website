6bd430e714dc706a0170d71be99f6ce2daa31d76
samonuall <samonuall@gmail.com>
2023-04-04 22:49:01 -0400

    add error message

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -5,15 +5,20 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <Dropdown placeHolder={"Faculty"} options={["Person A", "Person B", "Person C"]} />
+            <Dropdown placeHolder={"Button"} options={[]} />
             <LinkButton buttonText={"Home"} />
         </nav>
     );
 }
 
 /*
-    Component shows placeholder button, then when the user hovers over it the dropdown menu appears 
+    Component shows placeholder button, then when the user hovers over it the dropdown menu appears.
+    Need input options to not be an empty array
 */
 function Dropdown({placeHolder, options}: {placeHolder: string, options: string[]}) {
+    if(options.length == 0) {
+        throw new Error("Dropdown menu must have at least one option");
+    }
     const [showDropdown, setShowDropdown] = useState(false);
     const buttonRef = useRef(null);
     let dropdown = options.map((text, i) => < LinkButton key={i} buttonText={text} />);
