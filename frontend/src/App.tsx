14c2da69089d62cdbf101733136f899b8b350e1d
nehaka02 <nehaka02@gmail.com>
2023-04-08 23:43:30 -0400

    front end oauth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,31 @@
 import './App.css';
-import Button from './Button'
+import { useEffect } from 'react';
+
+function handleCallbackResponse(response: { credential: string; }){
+  console.log("Encoded JWT ID Token: " + response.credential);
+}
+
+
 
 function App() {
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+    client_id: "839962788219-jk1rq8rtjr724dpv0lumqskd7l1sp3a8.apps.googleusercontent.com",
+    callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    );
+  },[])
+
   return (
-    <>
-      <Button 
-        border="none"
-        color="#a7b0c4"
-        height = "10px"
-        onClick={() => console.log("You clicked on the pink circle!")}
-        radius = "50%"
-        width = "10px"
-      />
-    </>
+    <div className = "App">
+      <div id= "signInDiv"></div>
+    </div>
   );
 }
 
 export default App;
+
