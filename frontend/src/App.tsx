e52bc1150b46f06d1f86401714537dcb9a985def
nehaka02 <nehaka02@gmail.com>
2023-04-09 00:05:21 -0400

    oauth frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
 import { useEffect } from 'react';
-import jwt_decode from "jwt-decode";
 
-function handleCallbackResponse(response){
+
+function handleCallbackResponse(response: { credential: string; }){
   console.log("Encoded JWT ID Token: " + response.credential);
-  var userObject = jwt_decode(response.credential);
-  console.log(userObject);
 }
 
 
@@ -13,14 +11,17 @@ function handleCallbackResponse(response){
 function App() {
   useEffect(() => {
     /* global google */
+    
     google.accounts.id.initialize({
     client_id: "839962788219-jk1rq8rtjr724dpv0lumqskd7l1sp3a8.apps.googleusercontent.com",
     callback: handleCallbackResponse
     });
+
     google.accounts.id.renderButton(
       document.getElementById("signInDiv"),
       {theme: "outline", size: "large"}
     );
+
   },[])
 
   return (
