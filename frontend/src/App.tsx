c130d741d4cfea2fd92773d1496aaa94868fe754
nehaka02 <nehaka02@gmail.com>
2023-04-09 00:11:36 -0400

    oauth frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { useEffect } from 'react';
-
+import jwt_decode from "jwt-decode";
 
 function handleCallbackResponse(response: { credential: string; }){
   console.log("Encoded JWT ID Token: " + response.credential);
+  var userObject = jwt_decode(response.credential);
+  console.log(userObject);
 }
 
 
@@ -11,7 +13,7 @@ function handleCallbackResponse(response: { credential: string; }){
 function App() {
   useEffect(() => {
     /* global google */
-    
+
     google.accounts.id.initialize({
     client_id: "839962788219-jk1rq8rtjr724dpv0lumqskd7l1sp3a8.apps.googleusercontent.com",
     callback: handleCallbackResponse
