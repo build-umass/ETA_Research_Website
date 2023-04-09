5af930f4e57bca0db1737f12381d865fabe28a32
nehaka02 <nehaka02@gmail.com>
2023-04-08 22:18:31 -0400

    Button changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,13 @@ import Button from './Button'
 function App() {
   return (
     <>
-      <h1>Colorful Custom Button Components</h1>
       <Button 
         border="none"
-        color="pink"
-        height = "200px"
+        color="#a7b0c4"
+        height = "10px"
         onClick={() => console.log("You clicked on the pink circle!")}
         radius = "50%"
-        width = "200px"
-        children = "I'm a pink circle!"
+        width = "10px"
       />
     </>
   );
