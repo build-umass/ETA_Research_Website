edafc75b9207357c5cb1abed2df9cc5af6ed7e3e
Armand Asnani <aasnani@umass.edu>
2023-04-14 14:29:04 -0400

    Updated login callback to authorize with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,47 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import jwt_decode from "jwt-decode";
 
+interface RequestIsAuth {
+  userEmail: string;
+}
+
 function App() {
   const [ user, setUser ] = useState({});
+  const [ isAuth, setIsAuth ] = useState(null);
 
   function handleCallbackResponse(response: { credential: string; }){
     console.log("Encoded JWT ID Token: " + response.credential);
     var userObject = jwt_decode(response.credential);
+    let authRequest: RequestIsAuth = { userEmail: userObject.email };
     console.log(userObject);
-    setUser(userObject);
+    console.log(authRequest);
+    fetch("http://localhost:3001/api/isAuth", {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        userEmail: userObject.email
+      })
+  }).then((response) => {
+    if (response.status == 200) {
+      console.log("User is authenticated");
+      setIsAuth(true);
+      setUser(userObject);
+    } else {
+      console.log("User is not authenticated");
+      setIsAuth(false);
+    }}).catch((error) => {
+      console.log("Error: " + error);
+    });
+
     document.getElementById("signInDiv").hidden = true;
   }
 
   function handleSignOut(event){
     setUser({});
     document.getElementById("signInDiv").hidden = false;
+    setIsAuth(null);
   }
 
   useEffect(() => {
@@ -36,10 +63,11 @@ function App() {
   return (
     <div className = "App">
       <div id= "signInDiv"></div>
-      {Object.keys(user).length != 0 &&
-        <button style={{marginBottom: 10 + 'px' }} onClick= {(e) => handleSignOut(e)}>Sign Out</button>
+      {isAuth != null &&
+        <button style={{marginBottom: 10 + 'px'}} onClick= {(e) => handleSignOut(e)}>Sign Out</button>
       }
-      {user &&
+      {isAuth == false && <p>Unauthorized</p>}
+      {user && isAuth == true &&
         <div>
           <img src = {user.picture} referrerPolicy="no-referrer"></img>
           <h3>{user.name}</h3>
